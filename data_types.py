789ba201c77e42082de323f34508f947c5f4283a
Fathia Mohammed <mohammedfo5429@gmail.com>
2019-03-04 09:34:07 -0500

    datatypes

--- data_types.py
+++ data_types.py
@@ -5,12 +5,35 @@
 #     - "windows_count" -> data_type: int, value: 8
 #     - "glare" -> data_type: boolean, value: True
 
+dictionary_classroom = {
+'chairs' : {
+        'data_type': int,
+        'value': 35
+        }
+'name' : {
+        'data_type': 'string',
+        'value': CR3
+        }
+'windows_count' : {
+        'data_type': int,
+        'value': 8
+     }
+'glare' : {
+    'data_type': 'boolean', 
+    'value': True
+}
+}
 
 # #2: Create a list called "num_list" withs values 1 through 4 in it:
 
-
+num_list = [1, 2, 3, 4 ]
 # #3: Create a list called "teacher_list" with the strings "zakk", "don",
 # "jimmy", and "hector" in it:
+
+teacher_list  = [
+
+    'zakk', 'don', 'jimmy', 'hector'
+]
 
 
 # Commit when you finish working on these questions!
